9a4172ffed41db5bd451826172c4f112849b8d59
ajax <kolbaska205@gmail.com>
2018-04-15 15:12:04 +0600

    asd

--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,23 @@
 import java.net.*;
 import java.io.*;
+import java.sql.SQLException;
 
 public class Server {
-    public static void main(String[] ar)    {
+    public static void main(String[] ar)  throws SQLException, ClassNotFoundException  {
+        DBUtil.setConnection();
         int port = 6666;
         try {
             ServerSocket ss = new ServerSocket(port);
             System.out.println("Waiting for a client...");
             Socket socket = ss.accept();
+            System.out.println("accepted ok");
+            InputStream sin = socket.getInputStream();
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataInputStream in = new DataInputStream(sin);
             String line = null;
             while(true) {
-                line = in.readUTF();
+                line = in.readLine();
+                System.out.println(line);
                 if(line.equals("all")){ // SELECT *
                     out.writeObject(DBUtil.getData());
                 }
